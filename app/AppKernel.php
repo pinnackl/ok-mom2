9ea44b3e41f530cc075fa8d59e0e14ba2fb7f770
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-11-09 08:27:59 +0100

    [feature] add easy admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOSChildBundle\FOSChildBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
