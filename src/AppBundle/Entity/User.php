49763724d607a8f7706de19543c7b3c90082c327
alexis thorel <alexis.thorel@gmail.com>
2016-11-14 21:02:28 +0100

    [feature] start upload profile picture

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,9 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Security\Core\Util\SecureRandom;
 
 /**
  * User
@@ -21,6 +24,19 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @Assert\File(maxSize="2048k")
+     * @Assert\Image(mimeTypesMessage="Please upload a valid image.")
+     */
+    protected $profilePictureFile;
+
+    private $tempProfilePicturePath;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $profilePicturePath;
 
 
     /**
@@ -41,5 +57,170 @@ class User extends BaseUser
     public function getCredentialsExpireAt()
     {
         return $this->credentialsExpireAt;
+    }
+
+    /**
+     * Sets the file used for profile picture uploads
+     * 
+     * @param UploadedFile $file
+     * @return object
+     */
+    public function setProfilePictureFile(UploadedFile $file = null) {
+        // set the value of the holder
+        $this->profilePictureFile       =   $file;
+         // check if we have an old image path
+        if (isset($this->profilePicturePath)) {
+            // store the old name to delete after the update
+            $this->tempProfilePicturePath = $this->profilePicturePath;
+            $this->profilePicturePath = null;
+        } else {
+            $this->profilePicturePath = 'initial';
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the file used for profile picture uploads
+     * 
+     * @return UploadedFile
+     */
+    public function getProfilePictureFile() {
+
+        return $this->profilePictureFile;
+    }
+
+    /**
+     * Set profilePicturePath
+     *
+     * @param string $profilePicturePath
+     * @return User
+     */
+    public function setProfilePicturePath($profilePicturePath)
+    {
+        $this->profilePicturePath = $profilePicturePath;
+
+        return $this;
+    }
+
+    /**
+     * Get profilePicturePath
+     *
+     * @return string 
+     */
+    public function getProfilePicturePath()
+    {
+        return $this->profilePicturePath;
+    }
+
+    /**
+     * Get the absolute path of the profilePicturePath
+     */
+    public function getProfilePictureAbsolutePath() {
+        return null === $this->profilePicturePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->profilePicturePath;
+    }
+
+    /**
+     * Get root directory for file uploads
+     * 
+     * @return string
+     */
+    protected function getUploadRootDir($type='profilePicture') {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir($type);
+    }
+
+    /**
+     * Specifies where in the /web directory profile pic uploads are stored
+     * 
+     * @return string
+     */
+    protected function getUploadDir($type='profilePicture') {
+        // the type param is to change these methods at a later date for more file uploads
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/user/profilepics';
+    }
+
+    /**
+     * Get the web path for the user
+     * 
+     * @return string
+     */
+    public function getWebProfilePicturePath() {
+
+        return '/'.$this->getUploadDir().'/'.$this->getProfilePicturePath(); 
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUploadProfilePicture() {
+        if (null !== $this->getProfilePictureFile()) {
+            // a file was uploaded
+            // generate a unique filename
+            $filename = $this->generateRandomProfilePictureFilename();
+            $this->setProfilePicturePath($filename.'.'.$this->getProfilePictureFile()->guessExtension());
+        }
+    }
+
+    /**
+     * Generates a 32 char long random filename
+     * 
+     * @return string
+     */
+    public function generateRandomProfilePictureFilename() {
+        $count                  =   0;
+        do {
+            $generator = new SecureRandom();
+            $random = $generator->nextBytes(16);
+            $randomString = bin2hex($random);
+            $count++;
+        }
+        while(file_exists($this->getUploadRootDir().'/'.$randomString.'.'.$this->getProfilePictureFile()->guessExtension()) && $count < 50);
+
+        return $randomString;
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     * 
+     * Upload the profile picture
+     * 
+     * @return mixed
+     */
+    public function uploadProfilePicture() {
+        // check there is a profile pic to upload
+        if ($this->getProfilePictureFile() === null) {
+            return;
+        }
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getProfilePictureFile()->move($this->getUploadRootDir(), $this->getProfilePicturePath());
+
+        // check if we have an old image
+        if (isset($this->tempProfilePicturePath) && file_exists($this->getUploadRootDir().'/'.$this->tempProfilePicturePath)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->tempProfilePicturePath);
+            // clear the temp image path
+            $this->tempProfilePicturePath = null;
+        }
+        $this->profilePictureFile = null;
+    }
+
+     /**
+     * @ORM\PostRemove()
+     */
+    public function removeProfilePictureFile()
+    {
+        if ($file = $this->getProfilePictureAbsolutePath() && file_exists($this->getProfilePictureAbsolutePath())) {
+            unlink($file);
+        }
     }
 }
